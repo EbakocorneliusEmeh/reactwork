c1bdd1da70d4fecaacda5e15adad9abc0d6b350e
Ebakocornelius <ebakocornelius@gmail.com>
2025-11-22 09:57:20 +0100

    titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,52 @@
+
+
 import { useEffect, useState } from 'react'
 import './App.css'
-// import { reactRefresh } from 'eslint-plugin-react-refresh';                                     
 
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
 
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
 
-const Card = ({title}) => {
-  const [ count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false); 
-  useEffect( () => {
-console.log(`${title} has been liked: ${hasLiked}`);
-  });
-  return(
-    <div className='Card'  >
-      <h2>{ title }</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-    </div>
-  )
-}
+  return (
+    <div className="Card">
+      <h2>{title}</h2>
+      <button 
+  className={hasLiked ? "btn-liked" : "btn-like"}
+  onClick={() => setHasLiked(!hasLiked)}
+>
+  {hasLiked ? "Liked" : "Like"}
+</button>
+
+<div style={{ marginTop: "10px" }}>
+  <p>Count: {count}</p>
+
+  <button className="btn-increase" onClick={() => setCount(count + 1)}>
+    Increase
+  </button>
+
+  <button className="btn-decrease" onClick={() => setCount(count - 1)}>
+    Decrease
+  </button>
+</div>
+
+ </div>
+  );
+};
 
 const App = () => {
-
-
-  return(
+  return (
     <div>
-      <h2>Fcntional Arrow component</h2>
-        <Card title="Star wars" rating={5} iscool={true} actors={[{name: 'Actors'}]}/>
-      <Card title="Avator" />
-      <Card title="The lion king" />
-    </div>
-  )
-   
-}
+      <h2>Functional Arrow Component</h2>
 
-export default App
+      <Card title="Star Wars" rating={5} iscool={true} actors={[{ name: 'Actors' }]} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  );
+};
+
+export default App;
