b2b2f0a699c03669dc6bb86b3d7abe26625d2947
Ebakocornelius <ebakocornelius@gmail.com>
2025-12-08 14:47:09 +0100

    qorking on responsiveness

--- src/component/detail.jsx
+++ src/component/detail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
+
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
